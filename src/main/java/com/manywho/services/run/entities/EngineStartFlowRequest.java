8a00f303d3a23c6edbea58929abf63d71835cc8c
Steve Wood <steve.wood@manywho.com>
2015-02-17 19:39:09 -0800

    Added the additional dependency to maven, changed the engine start request to inherit engine initialization request, added a lot more error handling around the flow service to make it easier to work out when things are inevitably misconfigured. Added logic so we don't need to have the version identifier also.

--- src/main/java/com/manywho/services/run/entities/EngineStartFlowRequest.java
+++ src/main/java/com/manywho/services/run/entities/EngineStartFlowRequest.java
@@ -1,35 +1,10 @@
 package com.manywho.services.run.entities;
 
-import com.manywho.sdk.entities.draw.flow.FlowId;
-import com.manywho.sdk.entities.run.EngineValueCollection;
-import com.manywho.sdk.entities.run.Request;
+import com.manywho.sdk.entities.run.EngineInitializationRequest;
 import com.manywho.sdk.entities.security.AuthenticationCredentials;
-import org.apache.commons.collections4.CollectionUtils;
 
-public class EngineStartFlowRequest implements Request {
-    private FlowId flowId;
-    private EngineValueCollection inputs;
+public class EngineStartFlowRequest extends EngineInitializationRequest {
     private AuthenticationCredentials authenticationCredentials;
-
-    public FlowId getFlowId() {
-        return flowId;
-    }
-
-    public void setFlowId(FlowId flowId) {
-        this.flowId = flowId;
-    }
-
-    public EngineValueCollection getInputs() {
-        return inputs;
-    }
-
-    public boolean hasInputs() {
-        return CollectionUtils.isNotEmpty(inputs);
-    }
-
-    public void setInputs(EngineValueCollection inputs) {
-        this.inputs = inputs;
-    }
 
     public AuthenticationCredentials getAuthenticationCredentials() {
         return authenticationCredentials;
--- src/main/java/com/manywho/services/run/services/FlowService.java
+++ src/main/java/com/manywho/services/run/services/FlowService.java
@@ -1,13 +1,14 @@
 package com.manywho.services.run.services;
 
 import com.manywho.sdk.RunService;
-import com.manywho.sdk.entities.run.EngineInitializationRequest;
 import com.manywho.sdk.entities.run.EngineInitializationResponse;
 import com.manywho.sdk.entities.run.EngineInvokeRequest;
 import com.manywho.sdk.entities.run.EngineInvokeResponse;
 import com.manywho.sdk.entities.run.elements.map.MapElementInvokeRequest;
+import com.manywho.sdk.entities.draw.flow.FlowResponse;
 import com.manywho.sdk.entities.security.AuthenticatedWho;
 import com.manywho.sdk.enums.InvokeType;
+import com.manywho.sdk.enums.StatusCode;
 import com.manywho.services.run.entities.EngineStartFlowRequest;
 
 import javax.inject.Inject;
@@ -17,26 +18,72 @@ public class FlowService {
     private RunService runService;
 
     public EngineInvokeResponse startFlow(EngineStartFlowRequest startFlowRequest, String tenantId) throws Exception {
-        EngineInitializationResponse engineInitializationResponse = this.runService.initializeFlow(null, null, tenantId, new EngineInitializationRequest() {{
-            setFlowId(startFlowRequest.getFlowId());
-            if (startFlowRequest.hasInputs()) {
-                setInputs(startFlowRequest.getInputs());
-            }
-        }});
-
         AuthenticatedWho authenticatedWho = null;
-        if (startFlowRequest.hasAuthenticationCredentials()) {
-            authenticatedWho = this.runService.login(null, tenantId, engineInitializationResponse.getStateId(), startFlowRequest.getAuthenticationCredentials());
+
+        if (startFlowRequest == null) {
+            throw new Exception("The StartFlowRequest object cannot be null.");
         }
 
-        return this.runService.executeFlow(null, authenticatedWho, tenantId, new EngineInvokeRequest() {{
-            setCurrentMapElementId(engineInitializationResponse.getCurrentMapElementId());
-            setInvokeType(InvokeType.Forward);
-            setMapElementInvokeRequest(new MapElementInvokeRequest() {{
-                setSelectedOutcomeId(null);
-            }});
-            setStateId(engineInitializationResponse.getStateId());
-            setStateToken(engineInitializationResponse.getStateToken());
+        if (tenantId == null || tenantId.isEmpty() == true) {
+            throw new Exception("The TenantId cannot be null or blank.");
+        }
+
+        if (startFlowRequest.getFlowId() == null) {
+            throw new Exception("The StartFlowRequest.FlowId parameter cannot be null.");
+        }
+
+        if (startFlowRequest.getFlowId().getId() == null || startFlowRequest.getFlowId().getId().isEmpty() == true) {
+            throw new Exception("The StartFlowRequest.FlowId.Id parameter cannot be null or blank.");
+        }
+
+        if (startFlowRequest.getFlowId().getVersionId() == null || startFlowRequest.getFlowId().getVersionId().isEmpty() == true) {
+            FlowResponse flowResponse = this.runService.loadFlow(null, null, tenantId, startFlowRequest.getFlowId().getId());
+
+            if (flowResponse == null) {
+                throw new Exception("A Flow could not be found for the provided identifier.");
+            }
+
+            startFlowRequest.setFlowId(flowResponse.getId());
+        }
+
+        EngineInitializationResponse engineInitializationResponse = this.runService.initializeFlow(null, null, tenantId, startFlowRequest);
+
+        if (engineInitializationResponse.getStatusCode().equalsIgnoreCase(StatusCode.Unauthorized.toString())) {
+            if (startFlowRequest.hasAuthenticationCredentials() == false) {
+                throw new Exception("The Flow is requesting authentication and credentials have not been provided.");
+            }
+
+            authenticatedWho = this.runService.login(null, tenantId, engineInitializationResponse.getStateId(), startFlowRequest.getAuthenticationCredentials());
+
+            if (authenticatedWho == null) {
+                throw new Exception("The authentication credentials provided did not login successfully for an unknown reason.");
+            }
+
+            if (authenticatedWho.getManyWhoUserId() == null || authenticatedWho.getManyWhoUserId().isEmpty() == true) {
+                throw new Exception("The authentication credentials provided did not login successfully.");
+            }
+
+            // Send back the state identifier so we re-use the already created state, but this time authenticated
+            startFlowRequest.setStateId(engineInitializationResponse.getStateId());
+
+            // Re-initialize the flow with the authentication details
+            engineInitializationResponse = this.runService.initializeFlow(null, authenticatedWho, tenantId, startFlowRequest);
+
+            if (engineInitializationResponse.getStatusCode().equalsIgnoreCase(StatusCode.Unauthorized.toString())) {
+                throw new Exception("The authentication credentials have an unknown problem. Authentication has failed on the second initialization.");
+            }
+        }
+
+        // Need to do this slightly longer form as the engine initialization request is not final
+        EngineInvokeRequest engineInvokeRequest = new EngineInvokeRequest();
+        engineInvokeRequest.setCurrentMapElementId(engineInitializationResponse.getCurrentMapElementId());
+        engineInvokeRequest.setInvokeType(InvokeType.Forward);
+        engineInvokeRequest.setMapElementInvokeRequest(new MapElementInvokeRequest() {{
+            setSelectedOutcomeId(null);
         }});
+        engineInvokeRequest.setStateId(engineInitializationResponse.getStateId());
+        engineInvokeRequest.setStateToken(engineInitializationResponse.getStateToken());
+
+        return this.runService.executeFlow(null, authenticatedWho, tenantId, engineInvokeRequest);
     }
 }
