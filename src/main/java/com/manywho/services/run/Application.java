0864f239f90244a5e1e840cd134d60379fb5aad2
Jonjo McKay <jonjo@jonjomckay.com>
2020-04-19 19:04:37 +0100

    FLOW-1564: Added stuff required for Kubernetes

--- src/main/java/com/manywho/services/run/Application.java
+++ src/main/java/com/manywho/services/run/Application.java
@@ -1,11 +1,17 @@
 package com.manywho.services.run;
 
 import com.manywho.sdk.services.BaseApplication;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/")
 public class Application extends BaseApplication {
+    static {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+    }
+
     public Application() {
         registerSdk()
                 .packages("com.manywho.services.run")
@@ -13,6 +19,6 @@ public class Application extends BaseApplication {
     }
 
     public static void main(String[] args) {
-        startServer(new Application(), "api/run/1");
+        startServer(new Application());
     }
 }
